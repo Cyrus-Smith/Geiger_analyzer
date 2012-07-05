3e5e54069b83c416b23cc8171875b511b46a65ba
Cyrus Smith <Cyrus.Smith@lafraze.net>
2012-07-05 10:04:49 +0200

    Run until an exit signal occurs, call processing fct, etc.

--- geiger.c
+++ geiger.c
@@ -1,15 +1,21 @@
 
-/* Geiger counter listener prototype - 2012-07-04
+/* Geiger counter listener prototype - 2012
  * by "Cyrus Smith" for "Le Projet Olduvaï"
- * See http://le-projet-olduvai.wikiforum.net
  *
+ * See http://le-projet-olduvai.wikiforum.net/t6044-projet-de-logiciel-pour-compteur-geiger-muller
+ *
+ * This code is under GNU GPLv3.
  */
 
 #include <assert.h>
+#include <errno.h>
+#include <signal.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
 #include <portaudio.h>
 
@@ -21,13 +27,27 @@
    An instance of this structure is used with the callback function
    that process the audio input. */
 struct countdata {
-	int count;
+	PaTime start_time;
+	uint64_t count;
 	int threshold; // detection threshold to filter noise
 	int16_t last_values[2]; /* remember the last values between calls to the
 				   callback fct */
+	PaTime last_spl_time;   /* timestamp for the last sample processed */
 	uint64_t sample_number; /* count the number of samples (= time) */
-	PaTime initial_time;
 };
+static const struct countdata init_cd = {0.0, 0, 0, {0,0}, 0.0, 0};
+
+
+/* Signal Handling */
+
+static sig_atomic_t quit = 0; // will be set to !0 by a signal asking to quit.
+
+static void
+exithandler(int signal)
+{
+	quit = 1;
+}
+
 
 /* More or less an extremely crude derivative function... */
 static int8_t
@@ -45,7 +65,7 @@ evolution(int32_t value1, int32_t value2, int threshold)
 
 
 /* Naive peak detection (both positive and negative ones).
-   note that as this function is called for every value,
+   Note that, as this function is called for every value,
    the call to evolution(a, b, th) is the same as the call
    to evolution(b, c, th) of the previous call to peakp().
    This could hence be optimized if needed.
@@ -55,10 +75,16 @@ peakp(int16_t a, int16_t b, int16_t c, int th)
 {
 	int8_t le = evolution(a, b, th);
 	int8_t ce = evolution(b, c, th);
+
+	/* Positive peaks */
 	if (le >= 0 && ce < 0 && b > th)
 		return true;
+
+#if 0
+	/* Negative peaks */
 	if (le <= 0 && ce > 0 && b < -th)
 		return true;
+#endif
 	return false;
 }
 
@@ -68,12 +94,11 @@ geiger_callback(const void *input, void *output, unsigned long frameCount,
 		PaStreamCallbackFlags status, void *ourData)
 {
 	static bool first = true;
-	static PaTime initial_time;
+	struct countdata *data = (struct countdata*) ourData;
 	if (first) {
-		initial_time = timeInfo->inputBufferAdcTime;
+		data->start_time = timeInfo->inputBufferAdcTime;
 		first = false;
 	}
-	struct countdata *data = (struct countdata*) ourData;
 
 	int16_t *in = (int16_t*) input;
 	(void) output; /* Prevent unused variable warning. */
@@ -81,11 +106,17 @@ geiger_callback(const void *input, void *output, unsigned long frameCount,
 	if (status & paInputOverflow)
 		fprintf(stderr, "Warning: input overflow\n");
 
+	// fprintf(stderr, "fC: %lu\n", frameCount);
+
 	int16_t prev0 = data->last_values[0];
 	int16_t prev1 = data->last_values[1];
-	PaTime time = timeInfo->inputBufferAdcTime - initial_time;
-	for(int i = 0; i < frameCount; i++) {
+	PaTime time = timeInfo->inputBufferAdcTime - data->start_time;
+	for (int i = 0; i < frameCount; i++) {
+		/* The actual sample rate seems to be only an approximation of
+		   SAMPLE_RATE, hence 'time + (double) i / SAMPLE_RATE' is only
+		   an estimation of the sampling time. */
 		if (peakp(prev0, prev1, in[i], data->threshold)) {
+			data->count++;
 			printf("%.4f\t%6d\n", time + (double) i / SAMPLE_RATE,
 			       prev1);
 		}
@@ -95,33 +126,37 @@ geiger_callback(const void *input, void *output, unsigned long frameCount,
 	}
 	data->last_values[0] = prev0;
 	data->last_values[1] = prev1;
+	data->last_spl_time = time + (frameCount - 1.0) / SAMPLE_RATE;
 
 	return 0;
 }
 
 
-
-int
-main(int argc, char *argv[])
+/* This is a placeholder for a function called regularly that could be used
+   to process or display the new data. */
+static void
+process_new_data(struct countdata *data)
 {
-	PaDeviceIndex dev_used = 0; /* To be initialized: will allow selection
-				       of the soundcard / audio device used. */
-	int threshold = 4; /* The detection threshold should be set to a "good"
-			      default value, with a way for the user to specify
-			      a different value. */
+	static uint64_t total_count;
+	static PaTime prev_time;
+
+	if (data->count == 0)
+		return;
+
+	total_count += data->count;
+
+	fprintf(stderr, "current rate: %f /s\n",
+		data->count / (data->last_spl_time - prev_time));
 
 
-	/* Initialize portaudio */
+	prev_time = data->last_spl_time;
+	data->count = 0;
+}
 
-	PaError perr = Pa_Initialize();
-	if (perr != paNoError) {
-		fprintf(stderr, "Pa_Initialize failed: %s\n",
-			Pa_GetErrorText(perr));
-		return EXIT_FAILURE;
-	}
 
-	/* For fun: display the available audiodevices (= soundcards?) */
-
+static void
+list_sound_devices(void)
+{
 	PaDeviceIndex nb_dev = Pa_GetDeviceCount();
 	assert(nb_dev >= 0);
 	fprintf(stderr, "%d device(s) found\n", nb_dev);
@@ -132,39 +167,107 @@ main(int argc, char *argv[])
 		fprintf(stderr, "device %d: %s; %d input channel(s)\n", i + 1,
 			dev->name, dev->maxInputChannels);
 	}
+}
 
+static void
+global_init(void)
+{
+	{ /* Redirecting SIGINT & SIGTERM */
+		void (*oldh)();
+		oldh = signal(SIGINT, &exithandler);
+		if (oldh == SIG_ERR) {
+			fprintf(stderr, "signal failed to redirect INT signal:"
+				" %s\n", strerror(errno));
+			exit(EXIT_FAILURE);
+		}
+		oldh = signal(SIGTERM, &exithandler);
+		if (oldh == SIG_ERR) {
+			fprintf(stderr, "signal failed to redirect TERM signal:"
+				" %s\n", strerror(errno));
+			exit(EXIT_FAILURE);
+		}
+	}
 
+	/* Initialize portaudio */
 
+	PaError perr = Pa_Initialize();
+	if (perr != paNoError) {
+		fprintf(stderr, "Pa_Initialize failed: %s\n",
+			Pa_GetErrorText(perr));
+		exit(EXIT_FAILURE);
+	}
+}
 
-	struct countdata cdata = {0, threshold, {0,0}, 0, 0.0};
+static void
+global_finishup(void)
+{
+	PaError perr = Pa_Terminate();
+	if (perr != paNoError) {
+		fprintf(stderr, "Pa_Terminate failed: %s\n",
+			Pa_GetErrorText(perr));
+		exit(EXIT_FAILURE);
+	}
+}
 
+int
+main(int argc, char *argv[])
+{
+	PaDeviceIndex dev_used = 0; /* To be initialized: will allow selection
+				       of the soundcard / audio device used. */
+	int threshold = 4; /* The detection threshold should be set to a "good"
+			      default value, with a way for the user to specify
+			      a different value. */
 
-	/* open the input stream */
-	PaStreamParameters stream_params = { dev_used, 1, paInt16,
-					     Pa_GetDeviceInfo(dev_used)->defaultLowInputLatency,
-					     NULL };
+	global_init();
+
+	/* For fun: display the available audiodevices (= soundcards?) */
+	list_sound_devices();
+
+	PaError perr;
 	PaStream *stream;
-	perr = Pa_OpenStream(&stream, &stream_params, NULL, SAMPLE_RATE,
-			     paFramesPerBufferUnspecified, paNoFlag, geiger_callback,
-			     &cdata);
-	if (perr != paNoError) {
-		fprintf(stderr, "Pa_OpenStream failed: %s\n",
-			Pa_GetErrorText(perr));
-		return EXIT_FAILURE;
+	struct countdata cdata = init_cd;
+	cdata.threshold = threshold;
+	{ /* Open the input stream. */
+		PaTime latency = Pa_GetDeviceInfo(dev_used)->defaultLowInputLatency;
+		PaStreamParameters stream_params = { dev_used, 1, paInt16,
+						     latency, NULL };
+
+		perr = Pa_OpenStream(&stream, &stream_params, NULL, SAMPLE_RATE,
+				     paFramesPerBufferUnspecified, paNoFlag,
+				     geiger_callback, &cdata);
+		if (perr != paNoError) {
+			fprintf(stderr, "Pa_OpenStream failed: %s\n",
+				Pa_GetErrorText(perr));
+			return EXIT_FAILURE;
+		}
+
+		perr = Pa_StartStream(stream);
+		if (perr != paNoError) {
+			fprintf(stderr, "Pa_StartStream failed: %s\n",
+				Pa_GetErrorText(perr));
+			return EXIT_FAILURE;
+		}
 	}
 
 
+	/* listen & process the audio input until we are asked to exit. */
 
-	/* Temporary test: listen & process the audio input for 2 seconds. */
+	time_t t0 = time(NULL);
 
-	perr = Pa_StartStream(stream);
-	if (perr != paNoError) {
-		fprintf(stderr, "Pa_StartStream failed: %s\n",
-			Pa_GetErrorText(perr));
-		return EXIT_FAILURE;
+	while(!quit) {
+		Pa_Sleep(1000);
+		process_new_data(&cdata);
 	}
 
-	Pa_Sleep(2 * 1000);
+	time_t t1 = time(NULL);
+
+	/* Dump some last accounting data, close the stream, finishup & exit. */
+
+	double time = difftime(t1, t0);
+
+	fprintf(stderr, "%lu samples processed in %.0f seconds (%f spl/s)\n",
+		(long unsigned int) cdata.sample_number, time,
+		cdata.sample_number/time);
 
 	perr = Pa_StopStream(stream);
 	if (perr != paNoError) {
@@ -172,11 +275,6 @@ main(int argc, char *argv[])
 			Pa_GetErrorText(perr));
 		return EXIT_FAILURE;
 	}
-
-
-	fprintf(stderr, "%lu samples\n", cdata.sample_number);
-
-	/* Close the stream and terminate properly portaudio. */
 
 	perr = Pa_CloseStream(stream);
 	stream = NULL;
@@ -186,12 +284,7 @@ main(int argc, char *argv[])
 		return EXIT_FAILURE;
 	}
 
-	perr = Pa_Terminate();
-	if (perr != paNoError) {
-		fprintf(stderr, "Pa_Terminate failed: %s\n",
-			Pa_GetErrorText(perr));
-		return EXIT_FAILURE;
-	}
+	global_finishup();
 
 	return EXIT_SUCCESS;
 }
